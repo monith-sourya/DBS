365813d784e9af58f8a18c1a654f3f4fb5dd603e
msourya11@gmail.com <monith.sourya@gmail.com>
2018-04-19 01:59:16 +0530

    Change 1.0

--- ServerStuff.js
+++ ServerStuff.js
@@ -1,16 +1,31 @@
-import { WSAEPFNOSUPPORT } from "constants";
+//import { WSAEPFNOSUPPORT } from "constants";
+
 
 var mysql = require("mysql");
 var express = require("express");
 var app = express();
 
-console.log("Hello World");
+var http = require('http');
+
+function onRequest(request, response){
+    console.log("User Request" + request.url);
+
+    response.writeHead(200, {"Context-Type":"text/plain"});
+
+    response.write("Here is your response");
+
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('Server Running Successfully');
+
 
 var connect = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password = 'lescrutes@98',
-    database: 'sample'
+    password : 'Keyshore',
+    database: 'fitness'
 });
 
 connect.connect(function(error){
@@ -20,18 +35,25 @@ connect.connect(function(error){
     else{
         console.log('Connected');
     }
-});
 
-app.get('/', function(req, resp){
-    // about mysql
-    connect.query("Select * from sample", function(error, rows, fields){
-        if(!!error){
-            console.log('Error Wrong Query');
-        } else {
-            console.log('Successful Query');
-        }
+    connect.query("Select * from customer", function(err, result, fields){
+        if(err) throw err;
+
+        console.log(result);
     });
 });
+
+
+// app.get('/', function(req, resp){
+//     // about mysql
+//     connect.query("Select * from wingmates", function(error, rows, fields){
+//         if(!!error){
+//             console.log('Error Wrong Query');
+//         } else {
+//             console.log('Successful Query');
+//         }
+//     });
+// });
 
 app.listen()
 
