6387be52b45bd6d49b6fc879a96867e45b50a129
Laemons <ashanthrokser22@gmail.com>
2018-04-18 15:41:14 +0530

    Add files via upload

--- ServerStuff.js
+++ ServerStuff.js
@@ -1,1 +1,37 @@
+import { WSAEPFNOSUPPORT } from "constants";
+
+var mysql = require("mysql");
+var express = require("express");
+var app = express();
+
 console.log("Hello World");
+
+var connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password = 'lescrutes@98',
+    database: 'sample'
+});
+
+connect.connect(function(error){
+    if(error){
+        console.log('Error');
+    }
+    else{
+        console.log('Connected');
+    }
+});
+
+app.get('/', function(req, resp){
+    // about mysql
+    connect.query("Select * from sample", function(error, rows, fields){
+        if(!!error){
+            console.log('Error Wrong Query');
+        } else {
+            console.log('Successful Query');
+        }
+    });
+});
+
+app.listen()
+
