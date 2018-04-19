746519249ef0fedbb0ce17971d7f390ea1fe32ed
Monith <msourya11@gmail.com>
2018-04-20 01:46:40 +0530

    Add User Working on index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 //router.use(bodyParser());
+var mysql = require('mysql');
 
+var connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password : 'Keyshore',
+    database: 'fitness'
+});
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
@@ -16,7 +23,61 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.end(req.username); 
+
+	// console.log(req.body.username);
+	// console.log(req.body.sex);
+	// console.log(req.body.age);
+	// console.log(req.body.sub);
+	// console.log(req.body.subd);
+	
+
+connect.connect(function(error){
+    if(error){
+        console.log('Error in sql');
+    }
+    else{
+        console.log('Connected to sql');
+    }
+
+    var name = req.body.username;
+
+    connect.query("INSERT INTO customer (cust_id, cust_name, sex, age, sub_id, sub_dur, trainer_id, attendance, card_bal) VALUES (NULL,'"+req.body.username+"','"+ req.body.sex+"','"+ req.body.age+"', '1001','"+ req.body.subd+"','"+ req.body.trainer+"', '0', '0');",
+     function(err, result, fields){
+        if(err) throw err;
+        res.end("Your Customer ID is :"+result.insertId);
+    });
+    //res.end(JSON.stringify(req.body));
+});	
+
+// connect.connect(function(error){
+//     if(error){
+//         console.log('Error');
+//     }
+//     else{
+//         console.log('Connected');
+//     }
+
+//     connect.query("INSERT INTO `customer` (`cust_name`, `sex`, `age`, `sub_id`, `sub_dur`, `trainer_id`, `attendance`, `card_bal`) VALUES ('Monith', 'M', '11', '1001', '3', '1', '2', '0');",
+//      function(err, result, fields){
+//         if(err) throw err;
+//         res.end(JSON.stringify(req.body));
+//     });
+// });	
+
+// connect.connect(function(error){
+//     if(error){
+//         console.log('Error here');
+//     }
+//     else{
+//         console.log('Connected');
+//     }
+
+//     connect.query("Select * from customer", function(err, result, fields){
+//         if(err) throw err;
+
+//         console.log(result);
+//     });
+// });
 
 });
 
