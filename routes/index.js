ab2b90a42071c9c413ead3120fcfd2623b93247b
Monith <msourya11@gmail.com>
2018-04-19 18:55:39 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+//router.use(bodyParser());
+
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  res.end(req.username); 
+
 });
 
 module.exports = router;
