02dcd5e1e04fd435866e60dde39cb83823c01312
Monith <msourya11@gmail.com>
2018-04-25 19:39:44 +0530

    userdata on all res

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var connect = mysql.createConnection({
-    host: '192.168.0.13',
+    host: 'localhost',
     //port: '80', 
-    user: 'nikki',
+    user: 'root',
     password : 'Keyshore',
     database: 'fitness'
 });
