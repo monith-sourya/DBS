9c876757a47aba2e6cc320508304b957bac82e3f
Sanjay D <Sanjay D>
2018-04-27 12:34:07 +0530

    er diagram

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(bodyParser());
 
 var options = {
-    host: 'localhost',
+    host: '192.168.0.5',
    // port: '80',
     user: 'sanjay',
     password : 'Keyshore',
